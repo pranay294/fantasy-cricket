c8f235e9a99b2a34dcb7eed81296603fcf197452
Debloke Maitra <debloke.maitra@hpe.com>
2018-05-14 10:53:16 -0500

    Initial commit

---
+++ src/main/java/com/fantasy/cricket/FantasyCricketApplication.java
@@ -0,0 +1,11 @@
+package com.fantasy.cricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyCricketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyCricketApplication.class, args);
+    }
+}
---
+++ src/main/java/com/fantasy/cricket/controller/ApplicationDetails.java
@@ -0,0 +1,7 @@
+package com.fantasy.cricket.controller;
+
+/**
+ * Created by maitrad on 5/14/2018.
+ */
+public class ApplicationDetails {
+}
